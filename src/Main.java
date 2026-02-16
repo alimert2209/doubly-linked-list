0040aea5db2acb57ecbbcbc61610944534ea2848
mertho.k <alimert3215@gmail.com>
2026-02-16 10:21:59 +0300

    Main class created to do test.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        MusicPlayer musicPlayer = new MusicPlayer();
+        musicPlayer.addSong("Tarkan");
+        musicPlayer.addSong("Sebnem");
+        musicPlayer.printList();
+    }
+}
--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -2,6 +2,7 @@ public class MusicPlayer {
     DLNode head;
     DLNode tail;
 
+    // add song to end
     public void addSong(String songName){
         DLNode newSong = new DLNode(songName);
 
@@ -18,7 +19,7 @@ public class MusicPlayer {
 
     public void printList(){
         DLNode temp = head;
-        while (temp.next != null){
+        while (temp != null){
             System.out.print(temp.data + " <-> ");
             temp = temp.next;
         }
