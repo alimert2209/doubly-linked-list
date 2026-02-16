44b90dabf44e5a56540ef3e6f682ab7de94473ea
mertho.k <alimert3215@gmail.com>
2026-02-16 11:08:16 +0300

    playNext and playPrev functions added.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
 public class Main {
     public static void main(String[] args) {
         MusicPlayer musicPlayer = new MusicPlayer();
-        musicPlayer.addSong("Tarkan");
-        musicPlayer.addSong("Sebnem");
+        musicPlayer.playNext();
+        musicPlayer.playPrev();
+        musicPlayer.addSong("Tarkan - Kuzu Kuzu");
+        musicPlayer.addSong("Sebnem Ferah - Yagmurlar");
+        musicPlayer.addSong("Baris Manco - Donence");
+
+
+        musicPlayer.playPrev();
+        musicPlayer.playNext();
+        musicPlayer.playPrev();
+        musicPlayer.playNext();
+        musicPlayer.playNext();
+        musicPlayer.playNext();
+
         musicPlayer.printList();
+
+//        OUTPUT
+//
+//        There is no music in Music Player!
+//        There is no music in Music Player!
+//        This is the first song on the list. There are no previous ones.
+//        Playing: Sebnem Ferah - Yagmurlar
+//        Playing: Tarkan - Kuzu Kuzu
+//        Playing: Sebnem Ferah - Yagmurlar
+//        Playing: Baris Manco - Donence
+//        End of the list!
+//        Tarkan - Kuzu Kuzu <-> Sebnem Ferah - Yagmurlar <-> Baris Manco - Donence <-> End
+
     }
 }
--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -1,6 +1,7 @@
 public class MusicPlayer {
     DLNode head;
     DLNode tail;
+    DLNode currentSong;
 
     // add song to end
     public void addSong(String songName){
@@ -10,6 +11,7 @@ public class MusicPlayer {
         if (head == null) {
             head = newSong;
             tail = newSong;
+            currentSong = newSong; // first song is gonna be the current song
         } else {
             tail.next = newSong;
             newSong.prev = tail; // to not lose old tail
@@ -24,5 +26,35 @@ public class MusicPlayer {
             temp = temp.next;
         }
         System.out.println("End");
+    }
+
+    public void playNext(){
+        if (currentSong == null) {
+            System.out.println("There is no music in Music Player!");
+            return;
+        }
+
+        if (currentSong.next == null) {
+            System.out.println("End of the list!");
+            return;
+        }
+
+        currentSong = currentSong.next;
+        System.out.println("Playing: " + currentSong.data);
+    }
+
+    public void playPrev(){
+        if (currentSong == null) {
+            System.out.println("There is no music in Music Player!");
+            return;
+        }
+
+        if (currentSong.prev == null) {
+            System.out.println("This is the first song on the list. There are no previous ones.");
+            return;
+        }
+
+        currentSong = currentSong.prev;
+        System.out.println("Playing: " + currentSong.data);
     }
 }
